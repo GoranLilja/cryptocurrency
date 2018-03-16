ac23aa6f98f21d4f4ed6a49815602a7c0fca8fe7
Göran Lilja <goran.lilja@visma.com>
2018-03-16 13:20:36 +0100

    Added some logging. Clean up.

--- index.ts
+++ index.ts
@@ -3,15 +3,13 @@ declare function require(name: string)
 const sha256 = require("crypto-js/sha256")
 
 class Block {
-    index: number
     timestamp: number
     data: any
     previousHash: string
     hash: string
     nonce: number
 
-    constructor(index: number, timestamp: number, data: any, previousHash: string = "") {
-        this.index = index
+    constructor(timestamp: number, data: any, previousHash: string = "") {
         this.timestamp = timestamp
         this.data = data
         this.previousHash = previousHash
@@ -20,29 +18,32 @@ class Block {
     }
 
     calculateHash(): string {
-        return sha256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce).toString()
+        return sha256(this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce).toString()
     }
 
     mineBlock(difficulty: number) {
+        const startTime = +new Date()
+        console.log("Start mining block.")
         while (this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")) {
             this.nonce++
             this.hash = this.calculateHash()
         }
-        console.log(`Block mined. Hash: ${this.hash}. Nonce: ${this.nonce}.`)
+        const totalTime = (+(new Date()) - startTime) / 1000
+        console.log(`Block mined. Hash: ${this.hash}. Nonce: ${this.nonce}. Time spent: ${totalTime}s.`)
         
     }
 }
 
 class BlockChain {
     private chain: [Block]
-    private difficulty: number = 10
+    private difficulty: number = 5
     constructor() {
         this.chain = [this.createGenisisBlock()]
     }
 
     private createGenisisBlock(): Block {
         const now = +new Date()
-        return new Block(0, now, null, null)
+        return new Block(now, null, null)
     }
 
     getLatestBlock(): Block {
@@ -71,8 +72,8 @@ class BlockChain {
 
 const lilyCoin = new BlockChain()
 console.log("Mining block 1...")
-lilyCoin.addBlock(new Block(1, + new Date(), { amount: 100 }))
+lilyCoin.addBlock(new Block(+new Date(), { amount: 100 }))
 console.log("Mining block 2...")
-lilyCoin.addBlock(new Block(2, + new Date(), { amount: 120 }))
+lilyCoin.addBlock(new Block(+new Date(), { amount: 120 }))
 console.log("Done mining!")
 console.log(`Chain valid? ${lilyCoin.isChainValid() ? "Yes" : "No"}`)
